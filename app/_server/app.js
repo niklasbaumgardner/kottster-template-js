570275f8aca84a3d0d9d67f3ae9c83e3bdc8fb28
niklasbaumgardner <niklasbaumgardner@gmail.com>
2025-10-14 18:38:04 -0400

    Revert "Update app.js"
    This reverts commit b197e02be28362f1ada7ccf24de7c1517f3171a0.

--- app/_server/app.js
+++ app/_server/app.js
@@ -1,31 +1,27 @@
-import { getEnvOrThrow } from "@kottster/common";
-import { createApp, createIdentityProvider } from "@kottster/server";
-import schema from "../../kottster-app.json";
+import { createApp, createIdentityProvider } from '@kottster/server';
+import schema from '../../kottster-app.json';
 
-const SECRET_KEY = getEnvOrThrow("SECRET_KEY");
-const JWT_SECRET_SALT = getEnvOrThrow("JWT_SECRET_SALT");
-const ROOT_USER_PASSWORD = getEnvOrThrow("ROOT_USER_PASSWORD");
-
-/*
+/* 
  * For security, consider moving the secret data to environment variables.
  * See https://kottster.app/docs/deploying#before-you-deploy
  */
 export const app = createApp({
   schema,
-  secretKey: SECRET_KEY,
+  secretKey: '<your-secret-key-here>',
 
+  
   /*
    * The identity provider configuration.
    * See https://kottster.app/docs/app-configuration/identity-provider
    */
-  identityProvider: createIdentityProvider("sqlite", {
-    fileName: "app.db",
+  identityProvider: createIdentityProvider('sqlite', {
+    fileName: 'app.db',
 
-    passwordHashAlgorithm: "bcrypt",
-    jwtSecretSalt: JWT_SECRET_SALT,
-
+    passwordHashAlgorithm: 'bcrypt',
+    jwtSecretSalt: '<your-jwt-secret-salt-here>',
+    
     /* The root admin user credentials */
-    rootUsername: "admin",
-    rootPassword: ROOT_USER_PASSWORD,
+    rootUsername: 'admin',
+    rootPassword: 'admin',
   }),
 });
